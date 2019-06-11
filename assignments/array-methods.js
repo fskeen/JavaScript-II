f86746602abfb096fb199b2944bb439f1a9671f4
Faye Skeen <fskeen@gmail.com>
2019-06-11 13:46:00 -0700

    Completed array methods assignment + stretch goals.

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -2,7 +2,15 @@
 
 // Scroll to the bottom of the list to use some advanced array methods to help the event director gather some information from the businesses.
 
-const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"cseiler0@wired.com","shirt_size":"2XL","company_name":"Divanoodle","donation":75},
+const runners = [{
+    "id":1,
+    "first_name":"Charmain",
+    "last_name":"Seiler",
+    "email":"cseiler0@wired.com",
+    "shirt_size":"2XL",
+    "company_name":"Divanoodle",
+    "donation":75},
+
 {"id":2,"first_name":"Whitaker","last_name":"Ierland","email":"wierland1@angelfire.com","shirt_size":"2XL","company_name":"Wordtune","donation":148},
 {"id":3,"first_name":"Julieta","last_name":"McCloid","email":"jmccloid2@yahoo.com","shirt_size":"S","company_name":"Riffpedia","donation":171},
 {"id":4,"first_name":"Martynne","last_name":"Paye","email":"mpaye3@sciencedaily.com","shirt_size":"XL","company_name":"Wordware","donation":288},
@@ -55,29 +63,55 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
-let fullName = [];
-console.log(fullName);
+let fullName = []
+
+runners.forEach(function (a) {
+    return fullName.push(`${a.first_name} ${a.last_name}`)
+});
+
+console.log("Challenge 1: ", fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
-console.log(allCaps); 
+let allCaps = runners.map(a => a.first_name.toUpperCase());
+
+console.log("Challenge 2: ", allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-console.log(largeShirts);
+// let largeShirts = runners.filter(array => array.shirt_size === "L")
+
+let largeShirts = runners.filter(shirt => shirt.shirt_size === "L")
+
+console.log("Challenge 3: ", largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
+let ticketPriceTotal = runners.reduce(function (total, ticketprice) {
+    return total + ticketprice.donation;
+}, 0)
+
+console.log(`Challenge 4: $${ticketPriceTotal}`);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1: Use two methods to make an array of name tags with first name, last name, and the company listed on them.
 
-// Problem 2
+let nametag = runners.map(a => `Hello! My name is ${a.first_name} ${a.last_name}, from ${a.company_name}.`);
+console.log("Challenge 5, #1 :", nametag);
 
-// Problem 3
+// Problem 2: Use .filter to make a list of companies that donated over $100, and then print the list of companies sorted by donation amount.
+
+let topDonors = runners.filter(a => a.donation >= 100)
+                        .sort((a, b) => b.donation - a.donation);
+
+console.log("Challenge 5, #2: ", topDonors);
+
+// Problem 3: Create a thank you card for donors telling them thank you for their donation amount and listing how much was raised in total.
+
+let thankYous = runners.map(a => `Dear ${a.company_name},
+
+Thank you so much for your donation of $${a.donation}! With your help, we raised $${ticketPriceTotal} for charity.`);
+
+console.log("Challenge 5, #3: ", thankYous)
